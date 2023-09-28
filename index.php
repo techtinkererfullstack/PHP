6ad650b0fa87fdc2d928cbc30b29544876502e97
shafe <alam.shafee@gmail.com>
2023-09-28 21:47:45 +0600

    varibable, variable referance

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+#variable can be asigned with _ letter 
+$myName = "Shafe alam";
+
+#to show myname as variable we need to assigned it with double clots or {}
+echo 'hello $myname';
+echo "my name is $myName";
+
+echo 'my name is ' . $myName
+
+
+
+?>
+
+
+
+
