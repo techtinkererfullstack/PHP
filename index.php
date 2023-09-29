bc699e7b25523a8f76ef4846d823128172cf3d8e
Shafe Alam <77563020+techtinkererfullstack@users.noreply.github.com>
2023-09-29 18:50:03 +0600

    variable, constant, variable variable, magic variable

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sample php</title>
-</head>
-<body>
-    <h1>
-        <?php 
-        $myName = 'shafe alam';
-        echo 'My name is '. $myName
-        ?>
-    </h1>
-</body>
-</html>
+<?php 
+//variable
+$_myName = "tasnim"; //variable are assigned by value
+$_lastName = "alam"; 
+$_myName = &$_lastName; //variable assingned by referrence
+echo "my name is {$_myName}";
+
+
+//constant
+define('PROFESSION', 'software engineer');
+const STATUS_PAID = 'PAID';
+echo 'PROFESSTION : ' . PROFESSION;
+echo STATUS_PAID;
+
+defined("STATUS_PAID");
+
+
+//when have static data use constant
+
+//magic variables
+echo PHP_VERSION;
+echo __LINE__;
+
+//variable variable
+$name = 'tasnim';
+$$name = 'alam';
+
+echo "$name, {$$name}";
+
+
+        
+
 
 
 
